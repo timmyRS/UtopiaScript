756592201d56a4a4cb4c65600976e3b106f0b584
Tim "timmyRS" Speckhals <github@timmyrs.de>
2019-12-31 21:31:26 +0100

    Fix local_docs.sh

--- local_docs.sh
+++ local_docs.sh
@@ -3,6 +3,5 @@
 cd docs/ || exit 1
 gem install bundler jekyll
 bundler install
-cd .. || exit 1
 sleep 1 && start http://localhost:8035/ &
-jekyll serve -s docs -P 8035
+bundler exec jekyll serve -P 8035
